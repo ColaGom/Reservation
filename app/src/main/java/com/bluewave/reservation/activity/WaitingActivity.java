17f7b96d86dbe51d1af13ac8cce76edef8578f12
Developer <colabearstudio@gmail.com>
2016-05-16 06:13:29 +0900

    implements waiting activity

---
+++ app/src/main/java/com/bluewave/reservation/activity/WaitingActivity.java
@@ -0,0 +1,10 @@
+package com.bluewave.reservation.activity;
+
+import com.bluewave.reservation.base.BaseActivity;
+
+/**
+ * Created by Developer on 2016-05-16.
+ */
+public class WaitingActivity extends BaseActivity{
+
+}
